6584c6403677005a39dde2e92befd453e71ee614
Salim <ouimoinon@proton.me>
2025-07-28 20:43:05 +0200

    feat: better UI for posts preview

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -24,24 +24,24 @@ export function PostPreview({
 }: Props) {
   return (
     <div className="group flex flex-col h-full">
-      <div className="flex flex-col flex-grow">
-        <h3 className="text-xl md:text-2xl font-light mb-2 tracking-tight">
-          <p className="text-black hover:text-gray-700 transition-colors">
+      <div className="flex flex-col flex-grow gap-2">
+        <h3 className="text-lg font-semibold tracking-tight">
+          <p className="text-black/75 transition-colors">
             {title}
           </p>
         </h3>
-        <p className="text-sm text-gray-500 mb-3">
+        <p className="text-sm text-gray-500 italic">
           <DateFormatter dateString={date} />
         </p>
         {excerpt && (
-          <p className="text-gray-600 font-light mb-4 line-clamp-2">{excerpt}</p>
+          <p className="text-gray-600 font-light line-clamp-2">{excerpt}</p>
         )}
         <div className="mt-auto">
           <p 
             className="text-sm font-light text-black hover:text-gray-700 transition-colors inline-flex items-center gap-1 group"
           >
             Read article
-            <span className="transform translate-x-0 group-hover:translate-x-1 transition-transform duration-300">→</span>
+            <span className="transform translate-x-0 ml-2 group-hover:translate-x-4 transition-transform duration-300 h-1 w-8 bg-orange-400"></span>
           </p>
         </div>
       </div>
