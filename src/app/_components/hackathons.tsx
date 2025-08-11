b04e867adc6a3a57bafd44628032eb16dfadadda
Salim <ouimoinon@proton.me>
2025-08-11 20:01:27 +0200

    feat: add a hackathon

--- src/app/_components/hackathons.tsx
+++ src/app/_components/hackathons.tsx
@@ -7,7 +7,8 @@ export function Hackathons() {
     { id: 3, name: "ETHGlobal Cannes", date: "2025-07-04", description: "Description of the third hackathon", image: "/cannes.svg" },
     { id: 4, name: "Agentic Ethereum", date: "2023-04-01", description: "Description of the fourth hackathon", image: "/agentic.png" },
     { id: 5, name: "ETHGlobal Trifecta", date: "2023-05-01", description: "Description of the fifth hackathon", image: "/trifecta.svg" },
-    { id: 6, name: "Chiliz Hack Paris", date: "2025-07-01", description: "Description of the sixth hackathon", image: "/chiliz.svg" },
+    { id: 6, name: "Chiliz Hack Paris", date: "2025-07-01", description: "Chiliz Hack Paris", image: "/chiliz.svg" },
+    { id: 7, name: "World Wild Web Hack Night Paris", date: "2025-04-02", description: " Cloudflare and Fiberplane / HONC", image: "/f8967aa6-37ec-4044-bc3c-97cee161a1c7-_1_.png" },
   ];
 
   return (
@@ -21,11 +22,11 @@ export function Hackathons() {
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-fit">
                 {hackathons.map((hackathon) => (
                 <div key={hackathon.id} className="flex gap-4 hover:bg-blue-50 rounded-lg p-4">
-                    <div>
+                    <div className="w-48 h-32 flex-shrink-0 overflow-hidden rounded-md flex items-center justify-center">
                         <img
                             src={hackathon.image}
                             alt={hackathon.name}
-                            className="w-48 h-32 rounded-md p-4"
+                            className="rounded-md w-fit h-fit"
                             style={{ objectFit: "contain", background: "" }}
                         />
                     </div>
